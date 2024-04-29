a6628f58d432db56f7f4e166bf48c871a60e5285
Oskar Lesment <oskar-aleksander.lesment@tptlive.ee>
2024-04-29 16:57:56 +0300

    Checkbox redone , come basic css

--- src/App.vue
+++ src/App.vue
@@ -13,19 +13,21 @@
     <!--TASK-LIST CLASS-->
       <div class="task-list">
     <!--INDIVIDUAL TASK DIV-->
-        <div class="task-item" v-for="(task, taskIndex) in toDoList.toDoListTasksArray" :key="taskIndex"> 
+        <div class="task-item" v-for="task in toDoList.toDoListTasksArray" :key="task.id"> 
           <div class="checkbox-container">
             <input 
-              type="checkbox" 
-              :id="'checkbox-' + taskIndex"
-              :checked="toDoList.toDoListTasksArray[taskIndex].done" 
-              :key="taskIndex" 
-              v-on:click="toggleTaskStatus(toDoList, taskIndex)"
-              />
-            <label :for="'checkbox-' + taskIndex"><!--See ongi effectively mu checkbox, siia ei tohi midagi kirjutada, muidu renderdab CB sees. Siia ri tohi panna ka @ click, millel on sama nimi, mis on ka [lesandel, kuna label triggerdab ka CB [leval, mis siis omakorda eemaldab 2 list itemit--></label>
-            <div class="task-name" @click="toggleTaskStatus(toDoList, taskIndex)">{{ task.name }}</div>
+            type="checkbox" 
+            :id="'checkbox-' + task.id"
+            @click="toggleTaskStatus(toDoList, task)"
+            />
           </div>
-          <button>Placeholder</button>
+          <div class="task-name" 
+               @click="toggleTaskStatus(toDoList, task)">
+                {{ task.name }}
+          </div>
+          <div class="placeholder-btn-container">
+            <button>Placeholder</button>
+          </div>
         </div>
       </div>
     </div> 
@@ -60,9 +62,17 @@ export default {
       this.isToDoCreationModalVisible = true;
     },
     handleFreshToDoInApp(freshToDoList){
-      //tasks need to get on same state
-      freshToDoList.freshToDoListsTasksArray.forEach(task => {task.isEdited=false;}); 
+      let taskIdTicker=1;
+      //tasks need to get on same state which means i have to manually set is Edited to flase. 
+      freshToDoList.freshToDoListsTasksArray.forEach(function(task)
+      { task.isEdited=false;
+        //for checkboxes to work properly i need proper unique id, which otherwise vue or dom mismanage checkbox state. 
+        task.id=`${freshToDoList.freshToDoListTrimmedName}-${taskIdTicker}`;
+        taskIdTicker++;
+      }); 
+      //State Check
       console.log("this is fresh toDoList after adding iS Edited", freshToDoList);
+      //Maturing of fresh list to real list
       this.processedToDoList={
         toDoListName:        freshToDoList.freshToDoListName,
         toDoListClass:       freshToDoList.freshToDoListClass,
@@ -70,18 +80,25 @@ export default {
         toDoListTasksArray:  freshToDoList.freshToDoListsTasksArray
       };
       this.toDoListsBank.push(this.processedToDoList);
+      //State check
       console.log("This Is Processed To Do List", this.processedToDoList)
-      console.log("This Is state of the bank after processing", this.toDoListsBank); // check - whats in the Bank?
+      //State check
+      console.log("This Is state of the bank after processing", this.toDoListsBank); 
     },
     closeModal(){
       this.isToDoCreationModalVisible = false;
     },
-    toggleTaskStatus(toDoList, taskIndex){
-      toDoList.toDoListTasksArray[taskIndex].done = !toDoList.toDoListTasksArray[taskIndex].done;
-      if (toDoList.toDoListTasksArray[taskIndex].done===true) {
-        toDoList.toDoListTasksArray.splice(taskIndex, 1);
+    toggleTaskStatus(toDoList, task){
+      task.done = true;
+      //toDoList.toDoListTasksArray[taskIndex].done = !toDoList.toDoListTasksArray[taskIndex].done;
+      if (task.done===true) {
+         const index = toDoList.toDoListTasksArray.indexOf(task);
+      if (index > -1) {
+        toDoList.toDoListTasksArray.splice(index, 1);
       }
-      console.log(toDoList.toDoListTasksArray);
+    }
+    //State check
+    console.log(JSON.stringify(toDoList.toDoListTasksArray));
     }
     
 }
@@ -90,35 +107,40 @@ export default {
 </script>
 
 <style>
-
+#toDoLists{
+  max-width: 50%;
+}
 .task-list{
-  outline: 1px solid red;
-  display:grid;
-  grid-template-columns:auto 1fr auto;
-  align-items:center;
-  column-gap:10px;
+  max-width: 25%;
+  outline: 3px solid black;
+  display:flex;
+  flex-direction: column;
 }
 .task-item{
-  outline: 1px solid red;
+  display: inline-grid;
+  grid-template-columns: min-content 1fr min-content;
+  align-items: center;
+  gap: 10px;
+  outline: 3px dotted red;
+}
+.checkbox-container{
+  outline: 3px dashed darkgoldenrod;
+  display:grid;
+  place-items: center;
 }
 input[type="checkbox"]{
-  display:none;
-  
-}
-input[type="checkbox"]+label{
-  display:inline-block;
-  width:20px;
-  height:20px;
-  background-color:#fff;
-  border:1px solid #ccc;
+  transform: scale(1.5);
   cursor:pointer;
 }
-/* input[type="checkbox"]:checked+label{
-  background-color: #007bff;
-  border-color:#007bff;
-} */
-label{
-  margin:0;
+
+.placeholder-btn-container{
+  outline: 2px dashed yellow;
+}
+
+.task-item>.task-name{
+  text-align: center;
+  outline: 3px dashed rgb(30, 0, 255);
+  cursor:pointer;
 }
 
 
